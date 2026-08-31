74a830d7bcc2e2bde654cbc41dd4a95cc1aadede
Guido <gmellado@uct.cl>
2026-08-31 14:32:22 -0400

    seeder

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+from application.services.estudiante_service import EstudianteService
+from infrastructure.repositories.estudiante_repository import (
+    estudiante_repository,
+)
+from infrastructure.seeders.estudiante_seeder import seed_estudiantes
 from presentation.routers.estudiante_router import router
 
 
 app = FastAPI(title="Sistema Académico")
+
+service = EstudianteService(estudiante_repository)
+
+seed_estudiantes(service)
 
 app.include_router(router)
